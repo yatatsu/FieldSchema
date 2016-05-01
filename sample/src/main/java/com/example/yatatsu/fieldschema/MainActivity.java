792c36aa3e1e1c5760501268a24775a3f8dcf02c
yatatsu <yatatsukitagawa@gmail.com>
2016-05-01 19:22:53 +0900

    Add sample usage

--- sample/src/main/java/com/example/yatatsu/fieldschema/MainActivity.java
+++ sample/src/main/java/com/example/yatatsu/fieldschema/MainActivity.java
@@ -2,11 +2,27 @@ package com.example.yatatsu.fieldschema;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.yatatsu.fieldschema.FS;
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
+
+  private Realm realm;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    realm = Realm.getDefaultInstance();
+
+    RealmResults<Todo> todos = realm.where(Todo.class)
+        .equalTo(FS.mytodo_version, "1")
+        .contains(FS.mytodo_name, "high")
+        .findAllSorted(FS.mytodo_createdAt);
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    realm.close();
   }
 }
--- sample/src/main/java/com/example/yatatsu/fieldschema/Todo.java
+++ sample/src/main/java/com/example/yatatsu/fieldschema/Todo.java
@@ -1,11 +1,13 @@
 package com.example.yatatsu.fieldschema;
 
 import com.yatatsu.fieldschema.annotations.FieldSchemaClass;
+import io.realm.RealmModel;
+import io.realm.annotations.RealmClass;
 import java.util.Date;
 
-@FieldSchemaClass(name = "myTodo") public class Todo {
+@RealmClass @FieldSchemaClass(name = "myTodo") public class Todo implements RealmModel {
   public String name;
   public String description;
   public Date createdAt;
-  private String version;
+  public String version;
 }
