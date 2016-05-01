c7388e980be6cef157a37881be51daf0b7b527e1
yatatsu <yatatsukitagawa@gmail.com>
2016-05-01 20:23:03 +0900

    Add sample

---
+++ sample/src/main/java/com/example/yatatsu/fieldschema/TodoCategory.java
@@ -0,0 +1,9 @@
+package com.example.yatatsu.fieldschema;
+
+import com.yatatsu.fieldschema.annotations.FieldSchemaClass;
+
+@FieldSchemaClass public class TodoCategory {
+  public int id;
+  public String name;
+  public boolean enabled;
+}
